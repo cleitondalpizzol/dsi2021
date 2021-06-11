cf6267c94bbd88d85f65d716645e5e3e27e4a67a
cleitondalpizzol <cleitondalpizzol@hotmail.com>
2021-06-11 01:30:58 +0000

    inclui bootstrap

---
+++ src/main/java/br/univille/cleitondsi2021/controller/HomeController.java
@@ -0,0 +1,23 @@
+package br.univille.cleitondsi2021.controller;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ModelAndView index(){
+
+        HashMap<String,String> listaDeParzinho = new HashMap<>();
+        listaDeParzinho.put("nome", "Sistema de DSI 2021 - NAO ACREDITO");
+        listaDeParzinho.put("nomeEmpresa", "UNIVILLE SISTEMAS");
+
+        return new ModelAndView("home/index",listaDeParzinho);
+    }    
+}
