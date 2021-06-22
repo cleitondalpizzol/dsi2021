4a6a5d95bb1ee832d4c15a54a45eafe3a7f191d6
cleitondalpizzol <cleitondalpizzol@hotmail.com>
2021-06-22 01:24:48 +0000

    formulario

--- src/main/java/br/univille/cleitondsi2021/controller/EquipamentosController.java
+++ src/main/java/br/univille/cleitondsi2021/controller/EquipamentosController.java
@@ -3,9 +3,10 @@ package br.univille.cleitondsi2021.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQProperties.Packages;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,6 +25,13 @@ public class EquipamentosController {
         List<Equipamentos> listaEquipamentos = service.getALL();
         return new ModelAndView("equipamento/index", "listaEquipamentos", listaEquipamentos);
     }
-
-
+    @GetMapping("/novo")
+    public ModelAndView novo(@ModelAttribute Equipamentos equipamentos){
+        return new ModelAndView("equipamento/form");
+    }
+    @PostMapping(params ="form")
+    public ModelAndView save(Equipamentos equipamentos){
+        System.out.println(equipamentos.getNome());
+        return new ModelAndView("/equuipamento/form");
+    }
 }
